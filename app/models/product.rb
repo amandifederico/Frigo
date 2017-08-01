2882f496465d9ea8a549a0e0ebefd233d7884934
Silvio Geoffroy <sageoffroy@gmail.com>
2017-08-01 17:50:49 -0300

    Salvando el Dia

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
-  belongs_to :lot
+	has_many :product_details, inverse_of: :product
+	accepts_nested_attributes_for :product_details
+	belongs_to :lot
+
+	def to_s
+		description
+	end
 end
