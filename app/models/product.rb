87bdf68125e292041b9660fbda13c0e43e28d658
lockix <lockix@index.com>
2017-08-23 15:55:34 -0300

    textos y abm

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 	has_many :product_details, inverse_of: :product
-	accepts_nested_attributes_for :product_details
+	accepts_nested_attributes_for :product_details, :reject_if => proc { |attributes| attributes['weight'].blank? }
 	belongs_to :lot
 
 	def to_s
--- app/models/sub_product.rb
+++ app/models/sub_product.rb
@@ -1,5 +1,5 @@
 class SubProduct < ApplicationRecord
   has_many :sub_product_details, inverse_of: :sub_product
-  accepts_nested_attributes_for :sub_product_details
+  accepts_nested_attributes_for :sub_product_details, :reject_if => proc { |attributes| attributes['weight'].blank? }
   belongs_to :lot
 end
